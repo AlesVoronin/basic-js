916cfc3c9101f17ee4d46a716f13642d79ec0c1c
AlesVoronin <alesworonin@gmail.com>
2021-03-14 20:34:59 +0300

    feat: not completely done task "recursive depth"

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,7 +1,6 @@
-//const CustomError = require("../extensions/custom-error");
+const CustomError = require("../extensions/custom-error");
 
-//module.exports =
-    class DepthCalculator {
+module.exports = class DepthCalculator {
   calculateDepth(arr) {
     if (Array.isArray(arr)) return 1 + Math.max(...obj.map(t => arr(t)))
     else return 0
