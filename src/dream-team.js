ba0ffcb394dbd141a61562acba7bc59bb4f522e8
AlesVoronin <alesworonin@gmail.com>
2021-03-14 19:43:59 +0300

    feat: not completely done task "dream team"

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports =
+    function createDreamTeam(members) {
+  let result = "";
+  //members = members.filter(member => isNaN(member));
+  for (let i=0;i < members.length;i++) {
+    if (typeof members[i] != 'string'){
+      delete members[i]
+      continue
+    }
+    members[i] = members[i].replace(/\s+/g, '')
+  }
+  members = members.filter(member => member!= null)
+  members.sort();
+  for (let i=0;i < members.length;i++){
+    members[i] = members[i].toUpperCase();
+    result += members[i].charAt(0);
+  }
+  result.split('').sort().join();
+  if (result == '') return false
+  return result;
 };
