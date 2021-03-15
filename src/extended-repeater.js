9d754f10da433c4393273adf1e2524182feb5988
AlesVoronin <alesworonin@gmail.com>
2021-03-16 00:29:50 +0300

    feat: complete task "extended repeater"

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports =
+    function repeater(str, options) {
+
+      options.repeatTimes; // устанавливает набор number повторов str
+      options.separator; // это string разительное повторение str
+      options.addition; // является дополнительным, string которое будет добавляться к каждому повторению str
+      options.additionRepeatTimes; // устанавливает набор number повторов addition
+      options.additionSeparator; // это string разделительное повторение addition
+
+  let result_string = "";
+  str = String(str);
+  if (typeof options.addition != 'undefined') {
+    options.addition = String(options.addition);
+  }
+  if (!options.separator){
+    options.separator = "+";
+  }
+  if (!options.repeatTimes){
+    options.repeatTimes = 1;
+  }
+  if (!options.additionSeparator){
+    options.additionSeparator = "|";
+  }
+  if (!options.additionRepeatTimes){
+    options.additionRepeatTimes = 1;
+  }
+
+
+
+  for (let i=0; i < options.repeatTimes; i++){
+    result_string += str;
+    if (options.addition) {
+      for (let j = 0; j < options.additionRepeatTimes; j++) {
+        result_string += options.addition;
+        if (j!= options.additionRepeatTimes-1) {
+          result_string += options.additionSeparator
+        }
+      }
+    }
+    if (i!= options.repeatTimes-1) {
+      result_string += options.separator;
+    }
+  }
+  return result_string;
 };
   
