84aa5cbc681b61987c59c118abfa706c479c3f60
AlesVoronin <alesworonin@gmail.com>
2021-03-16 02:23:22 +0300

    feat: complete task "transform-array"

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,56 +1,55 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports =
-    function transform(arr) {
-  if (arr.length == 0) return arr;
-  for (let i=0; i < arr.length; i++){
-    if (arr[i] == "--discard-next"){
-        for (let j=i+1; j < arr.length; j++){
-          if (arr[j]){
-            arr.splice(j,1);
-            break;
-          }
+function transform(arr) {
+    if (arr.length == 0) return arr;
+    let not_empty = 0
+    for (let i =0; i < arr.length; i++){
+        if (arr[i]){
+          not_empty++;
         }
-        arr.splice(i,1);
-        continue;
     }
-    else if (arr[i] == "--discard-prev"){
-      for (let j=i-1; j > 0; j--){
-        if (arr[j]){
-          arr.splice(j,1);
-          break;
+    if(not_empty == 0){
+      return [];
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == "--discard-next") {
+            if (arr[i + 1]) {
+                arr.splice(i + 1, 1, null);
+            }
+            arr.splice(i, 1, null);
+            continue;
+        } else if (arr[i] == "--discard-prev") {
+            if (arr[i - 1]) {
+                arr.splice(i - 1, 1, null);
+            }
+            arr.splice(i, 1, null);
+            continue;
+        } else if (arr[i] == "--double-next") {
+            if (arr[i + 1] || arr[i + 1] == null || arr[i + 1] == 0) {
+                arr[i] = arr[i+1];
+            }
+            continue;
+        } else if (arr[i] == "--double-prev") {
+            if (arr[i - 1] || arr[i - 1] == null || arr[i + 1] == 0) {
+                arr[i] = arr[i-1];
+            }
+            continue;
         }
-      }
-      arr.splice(i,1);
-      continue;
     }
-    else if (arr[i] == "--double-next"){
-      for (let j=i+1; j < arr.length; j++){
-        if (arr[j]){
-          arr[j]*=2;
-          break;
-        }
-      }
-      arr.splice(i,1);
-      continue;
-    }
-    else if (arr[i] == "--double-prev"){
-      for (let j=i-1; j > 0; j--){
-        if (arr[j]){
-          arr[j]*=2;
-          break;
-        }
-      }
-      arr.splice(i,1);
-      continue;
-    }
-  }
-  clear(arr);
-  return arr
+    clear(arr);
+    return arr
 };
 
-function clear(arr){
-  arr = arr.filter(function(x) {
-    return x !== undefined && x !== null && x !== ""});
-  return arr
+function clear(arr) {
+    // arr = arr.filter(function (x) {
+    //     return x !== undefined && x !== null && x !== ""
+    // });
+    for (let i=0; i < arr.length; i++){
+      if (arr[i] == null || arr[i] == NaN || arr[i] == 0){
+        arr.splice(i,1);
+        i--;
+      }
+    }
+    return arr
 }
