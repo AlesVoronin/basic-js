2299fc2cbc21864256c2b58af5c90e7dc95aab1e
AlesVoronin <alesworonin@gmail.com>
2021-03-15 23:19:42 +0300

    feat: complete task "hanoi tower"

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports =
+    function transform(arr) {
+  if (arr.length == 0) return arr;
+  for (let i=0; i < arr.length; i++){
+    if (arr[i] == "--discard-next"){
+        for (let j=i+1; j < arr.length; j++){
+          if (arr[j]){
+            arr.splice(j,1);
+            break;
+          }
+        }
+        arr.splice(i,1);
+        continue;
+    }
+    else if (arr[i] == "--discard-prev"){
+      for (let j=i-1; j > 0; j--){
+        if (arr[j]){
+          arr.splice(j,1);
+          break;
+        }
+      }
+      arr.splice(i,1);
+      continue;
+    }
+    else if (arr[i] == "--double-next"){
+      for (let j=i+1; j < arr.length; j++){
+        if (arr[j]){
+          arr[j]*=2;
+          break;
+        }
+      }
+      arr.splice(i,1);
+      continue;
+    }
+    else if (arr[i] == "--double-prev"){
+      for (let j=i-1; j > 0; j--){
+        if (arr[j]){
+          arr[j]*=2;
+          break;
+        }
+      }
+      arr.splice(i,1);
+      continue;
+    }
+  }
+  clear(arr);
+  return arr
 };
+
+function clear(arr){
+  arr = arr.filter(function(x) {
+    return x !== undefined && x !== null && x !== ""});
+  return arr
+}
